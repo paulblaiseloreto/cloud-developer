23f420924f8318c279add5aa2941d9da398975e9
Paul Loreto <paulloreto@gmail.com>
2020-09-20 00:05:56 +0800

    commit for "npm run dev"

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,31 @@
 export const config = {
-  "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
-  },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  }
+    "postgress": {
+        "username": "udagramloretodev",
+        "password": "udagramloretodev",
+        "database": "udagramloretodev",
+        "host": "udagramloretodev.cdcv3tlk8exq.ap-southeast-1.rds.amazonaws.com",
+        "dialect": "postgress",
+    },
+    "aws": {
+        "aws_region": process.env.AWS_REGION,
+        "aws_profile": process.env.AWS_PROFILE,
+        "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
+    },
+    "dev": {
+        "username": "",
+        "password": "",
+        "database": "",
+        "host": "",
+        "dialect": "postgres",
+        "aws_region": "us-east-2",
+        "aws_profile": "default",
+        "aws_media_bucket": "udagram-ruttner-dev"
+    },
+    "prod": {
+        "username": "",
+        "password": "",
+        "database": "udagram_prod",
+        "host": "",
+        "dialect": "postgres"
+    }
 }
